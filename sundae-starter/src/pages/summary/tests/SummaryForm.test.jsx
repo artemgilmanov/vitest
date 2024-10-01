40208cb848a7665bbb333ecf7f534f78a1c87c9c
Artem Gilmanov <artem.gilmanov@mesalvo.com>
2024-10-01 09:10:38 +0200

    SummaryForm added test for hover.

--- sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
@@ -40,4 +40,26 @@ describe('summary form tests', () => {
     await user.click(checkbox);
     expect(confirmButton).toBeDisabled();
   });
+
+  test('popover responds to hover', async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm />);
+    // popover start out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // appears on mousover
+    const termsAndCoditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndCoditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    // dissapear when we mouse out
+    await user.unhover(termsAndCoditions);
+    expect(popover).not.toBeInTheDocument();
+  });
 });
